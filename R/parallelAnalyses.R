2f211eac818c2d8eece817ec2ccd96034967e2ab
U-John-PC\John L <7jlawson@gmail.com>
2018-03-23 10:41:16 -0400

    Init: alternative analyses to support results

---
+++ R/parallelAnalyses.R
@@ -0,0 +1,24 @@
+
+# parallel/confirmatory analyses
+library(data.table)
+
+# run cytosines with extreme loading values through LOLA
+setCacheDir(paste0(Sys.getenv("PROCESSED"), "brca_PCA/RCache/"))
+simpleCache("combinedBRCAMethyl_noXY")
+coordinates = combinedBRCAMethyl_noXY[["coordinates"]]
+
+simpleCache("allMPCA")
+
+mLoadings = allMPCA$rotation[, 1:6]
+
+hist(mLoadings[,"PC1"])
+pc1 = abs(mLoadings[, "PC1"])
+# highest 25% of cytosines
+pc1KeepCoords = coordinates[pc1 >= quantile(pc1, probs = 0.75), ]
+pc1KeepCoords[, end := start]
+write.table(x=pc1KeepCoords, file = "PC1.bed", quote = FALSE, sep = "\t",row.names = FALSE, col.names = FALSE)
+
+#PC2
+pc2KeepCoords = coordinates[pc1 >= quantile(pc1, probs = 0.75), ]
+pc2KeepCoords[, end := start]
+write.table(x=pc1KeepCoords, file = "PC1.bed", quote = FALSE, sep = "\t",row.names = FALSE, col.names = FALSE)
