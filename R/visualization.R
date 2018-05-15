ce9bd41a4a2f541e987cd6efc955327e9b58c54c
U-John-PC\John L <7jlawson@gmail.com>
2018-05-15 10:54:35 -0400

    Add heatmap function to compare enrichment scores across PCs

--- R/visualization.R
+++ R/visualization.R
@@ -1,5 +1,7 @@
 # functions to visualize results of PCRSA, relevant regions, and variation in the dataset
-
+# data.table: copy, :=, setorder
+# MIRA: dtToGr
+# ComplexHeatmap
 
 #' Function to look at methylation in regions of interest across patients, 
 #' with patients ordered according to PC of interest 
@@ -51,3 +53,55 @@ rsMethylHeatmap <- function(methylData, coordGR, regionSet, pcaData, pc="PC1", .
     ComplexHeatmap::Heatmap(thisRSMData, cluster_rows = FALSE, cluster_columns = FALSE,
                             use_raster=TRUE, raster_device = "png")
 }
+
+
+
+#' Heatmap of enrichment scores across PCs
+#' A visualization of the enrichment data.table.
+#' 
+#' @param rsEnrichment
+#' @param PCsToAnnotate A character vector with principal components to 
+#' include in the plot, eg c("PC1", "PC2")
+#' @param orderByPC PC to order by (decreasing order) in heatmap
+#
+#' @example enrichmentHeatmap = rsEnrichHeatmap(rsEnrichment, PCsToAnnotate=paste0("PC", 1:10), orderByPC = "PC2")
+rsEnrichHeatmap <- function(rsEnrichment, PCsToAnnotate=paste0("PC", 1:5),
+                            orderByPC="PC1", rsNameCol = "rsNames", topX = 20) {
+    
+    # so by reference operations will not affect original object
+    rsEn = data.table::copy(rsEnrichment)
+    
+    # only ones you have data for
+    PCsToAnnotate = PCsToAnnotate[PCsToAnnotate %in% colnames(rsEn)]
+    if (length(PCsToAnnotate) == 0) {
+        stop("Please check format of PC names in PCsToAnnotate.")
+    }
+     
+    rsEn = rsEn[, c(PCsToAnnotate, rsNameCol), with = FALSE] # apparently erases row names
+    
+    
+    # how to deal with NA?
+    
+    # number of region sets tested
+    rsNum = nrow(rsEn)
+    
+    for (i in seq_along(PCsToAnnotate)) {
+        # first convert to rank
+        setorderv(rsEn, PCsToAnnotate[i], order = -1L) # descending order
+        rsEn[, PCsToAnnotate[i] := 1:rsNum]
+        
+        # center around zero
+        rsEn[, PCsToAnnotate[i] := ((rsNum + 1) / 2) - get(PCsToAnnotate[i])]
+    }
+    
+    # heatmap of the centered ranks
+    setorderv(rsEn, orderByPC, order = -1L) # back to first order
+    rowNames =  rsEn[, get(rsNameCol)] # redefined/reordered later
+    row.names(rsEn) <- rowNames
+    rsEn[, c(rsNameCol) := NULL]
+    rsEn = as.matrix(rsEn)
+    row.names(rsEn) <- rowNames
+    Heatmap(rsEn[1:topX, ], cluster_rows = FALSE, cluster_columns = FALSE, show_row_names = TRUE, row_names_max_width = unit(100000, "mm"))
+    
+}
+
