15571fd1682fdada21734c5113f2fdb4297bf59a
j-lawson <jtl2hk@virginia.edu>
2018-07-05 15:29:34 -0400

    Add package data for the vignette

--- R/data.R
+++ R/data.R
@@ -1,3 +1,95 @@
+#' A matrix with loadings
+#' 
+#' This object contains loadings
+#' for PCA of DNA methylation data.
+#' DNA methlyation data is from breast cancer
+#' patients from The Cancer Genome Atlas
+#' (TCGA-BRCA, https://portal.gdc.cancer.gov/).
+#' Each row corresponds to one cytosine and
+#' the coordinates for these cytosines are
+#' in the object brcaCoords1. Only
+#' cytosines on chr1 are included to keep
+#' the example data small.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name brcaLoadings1
+#' @usage data(brcaLoadings1)
+#' @format A matrix object
+NULL
+
+#' A data.table, data.frame object with coordinates for cytosines
+#' from chr1 included in the PCA. 
+#' 
+#' Corresponds to 
+#' the rows of brcaLoadings1.
+#' DNA methlyation data is from breast cancer
+#' patients from The Cancer Genome Atlas
+#' (TCGA-BRCA, https://portal.gdc.cancer.gov/).
+#' Only cytosines on chr1 are included to keep
+#' the example data small.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name brcaCoord1
+#' @usage data(brcaCoord1)
+#' @format A data.table, data.frame object
+NULL
+
+#' Estrogen receptor alpha binding regions.
+#' 
+#' Binding regions for estrogen receptor alpha (ESR1).
+#' Only includes regions in chr1 to keep
+#' the example data small.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name esr1_chr1
+#' @usage data(esr1_chr1)
+#' @format A GRanges object
+NULL
+
+#' Gata3 binding regions.
+#' 
+#' Binding regions for gata3.
+#' Only includes regions in chr1 to keep
+#' the example data small.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gata3_chr1
+#' @usage data(gata3_chr1)
+#' @format A GRanges object
+NULL
+
+#' Nrf1 binding regions.
+#' 
+#' Binding regions for Nrf1.
+#' Only includes regions in chr1 to keep
+#' the example data small.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name nrf1_chr1
+#' @usage data(nrf1_chr1)
+#' @format A GRanges object
+NULL
+
+#' Atf3 binding regions.
+#' 
+#' Binding regions for Atf3.
+#' Only includes regions in chr1 to keep
+#' the example data small.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name atf3_chr1
+#' @usage data(atf3_chr1)
+#' @format A GRanges object
+NULL
+
 # # script for generating package data
 # # restricting data to reduce how much memory the package takes up
 # 
@@ -15,8 +107,8 @@
 # 
 # # region sets
 # # some of the top ranked region sets for PC1 (in top 5)
-# er = GRList[rsName == "Human_MCF-7_ESR1_E2-45min_Brown.bed"][[1]]
-# gata = GRList[rsName == "wgEncodeAwgTfbsSydhMcf7Gata3UcdUniPk.narrowPeak"][[1]]
+# esr1 = GRList[rsName == "Human_MCF-7_ESR1_E2-45min_Brown.bed"][[1]]
+# gata3 = GRList[rsName == "wgEncodeAwgTfbsSydhMcf7Gata3UcdUniPk.narrowPeak"][[1]]
 # # some of the bottom ranked region sets for PC1 (in bottom 1% of >2200 region sets)
 # nrf1 = GRList[rsName == "wgEncodeAwgTfbsSydhHepg2Nrf1IggrabUniPk.narrowPeak"][[1]]
 # atf3 = GRList[rsName == "wgEncodeAwgTfbsSydhK562Atf3UniPk.narrowPeak"][[1]]
@@ -34,13 +126,27 @@
 # 
 # # restrict to chromosome 1 (reduce file sizes)
 # chr1Ind = coordinateDT$chr == "chr1"
-# coord1 = coordinateDT[chr1Ind,]
+# brcaCoord1 = coordinateDT[chr1Ind,]
 # # subset loadings, also restrict PCs
-# load1 = loadingMat[chr1Ind, paste0("PC", 1:4)]
+# brcaLoadings1 = loadingMat[chr1Ind, paste0("PC", 1:4)]
 # 
-# 
-# grList = GRangesList(er, gata, nrf1, atf3)
+# # restrict region sets to chr1
+# grList = GRangesList(er, gata3, nrf1, atf3)
 # ol1 = lapply(X = grList, function(x) findOverlaps(query = x, subject = MIRA:::dtToGr(coord1)))
 # 
-# save("coord1", file = "coord1.RData") # reduce ~4x from in-memory size
-# save("coord1", file = "coord1.RData", compress = "xz") # reduce ~7x
+# # to restrict to chr1
+# esr1_chr1 = esr1[ seqnames(esr1) == "chr1"]
+# gata3_chr1 = gata3[ seqnames(gata3) == "chr1"]
+# nrf1_chr1 = nrf1[ seqnames(nrf1) == "chr1"]
+# atf3_chr1 = atf3[ seqnames(atf3) == "chr1"]
+# 
+# # save("", file = "coord1.RData") # reduce ~4x from in-memory size
+# save("brcaLoadings1", file = "brcaLoadings1.RData", compress = "xz") # reduce ~7x
+# save("brcaCoord1", file = "brcaCoord1.RData", compress = "xz")
+# 
+# save("esr1_chr1", file = "esr1_chr1.RData", compress = "xz")
+# save("gata3_chr1", file = "gata3_chr1.RData", compress = "xz")
+# save("nrf1_chr1", file = "nrf1_chr1.RData", compress = "xz")
+# save("atf3_chr1", file = "atf3_chr1.RData", compress = "xz")
+
+
