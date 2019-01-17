9990ea68100939720ff99654ab7bf42c528793d7
j-lawson <jtl2hk@virginia.edu>
2019-01-17 16:50:09 -0500

    Fix bug in new function

--- R/COCOA.R
+++ R/COCOA.R
@@ -764,6 +764,7 @@ getTopRegions <- function(loadingMat,
     
     regionLoadDT = COCOA:::averageByRegion(loadingMat=loadingMat,
                             signalCoord=signalCoord, regionSet=regionSet, 
+                            PCsToAnnotate = PCsToAnnotate,
                             returnQuantile = returnQuantile)[]
     
     keepInd = regionLoadDT[, PCsToAnnotate, with=FALSE] >= cutoff
