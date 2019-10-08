09b5b203afea96628152752305ec0dcef76b5cb2
j-lawson <jtl2hk@virginia.edu>
2019-10-08 12:48:55 -0400

    Doc updates

--- man-roxygen/signalCoordType.R
+++ man-roxygen/signalCoordType.R
@@ -3,7 +3,7 @@
 #' (`signalCoord`) are each a single base (e.g. as for DNA methylation)
 #' or a region/multiple bases (e.g. as for ATAC-seq). Different scoring
 #' options are available for each type of data. If "default" is given,
-#' the type of coordinates will be detected automatically. If each
+#' the type of coordinates will be detected automatically. For "default", if each
 #' coordinate start value equals the coordinate end value 
 #' (all(start(signalCoord) == end(signalCoord))), "singleBase"
 #' will be used. Otherwise, "multiBase" will be used.
