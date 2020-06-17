694e4b49662ca0a28701421a81a19e4ed88e52f9
j-lawson <jtl2hk@virginia.edu>
2020-06-16 22:52:09 -0400

    Update documentation

---
+++ man-roxygen/checkInput.R
@@ -0,0 +1,5 @@
+#' @param .checkInput A "logical" object. For programmatic use only.
+#' Whether inputs to the function should be checked for 
+#' correctness/appropriateness. This parameter may be used by some COCOA
+#' functions to prevent unnecessary checks of objects 
+#' after arguments have already been checked once.  
--- man-roxygen/olList.R
+++ man-roxygen/olList.R
@@ -5,7 +5,9 @@
 #' and signalCoord must be the "query". E.g. findOverlaps(subject=regionSet,
 #' query=signalCoord).
 #' Providing this information can greatly improve permutation speed since the 
-#' overlaps will not have to be calculated for each permutation. When using 
+#' overlaps will not have to be calculated for each permutation. 
+#' The "runCOCOAPerm" function calculates this information only once, internally,
+#' so this does not have to be provided when using that function. When using 
 #' this parameter, signalCoord, 
 #' genomicSignal, and each region set must be in the same order as they were
 #' when olList was created. Otherwise, the wrong genomic loci will be referenced
