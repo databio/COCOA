258c5e8c1e55ee266dcb2ea7062a999896feec92
Kinga Nimz <kinga@nimz.pl>
2023-08-07 12:12:37 -0400

    Update rsScores.R

--- man-roxygen/rsScores.R
+++ man-roxygen/rsScores.R
@@ -1,10 +1,9 @@
-#' @param rsScores data.frame. A data.frame with region set
-#' scores. The output of the 'aggregateSignalGRList' function.
-#' Each row is a region set. One column for each sample
-#' variable of interest (e.g. PC or sample phenotype).
-#' Also can have columns with info on the overlap between the 
-#' region set and the epigenetic data. 
-#' Rows should be in the same order as the region sets in GRList
+#' @param rsScores data.frame containing region set scores from 
+#' the 'aggregateSignalGRList' function. Each row represents a region set, 
+#' and columns contain sample variables of interest (e.g., PC or sample phenotype).
+#' Additionally, it can have columns with overlap information between the region set 
+#' and epigenetic data.
+#' Make sure that the rows are in the same order as the region sets in GRList.
 #' (the list of region sets used to create rsScores.)
 # (runCOCOAPerm) Must include columns with names given by 'colsToAnnotate'.
 # (getPermStat)
